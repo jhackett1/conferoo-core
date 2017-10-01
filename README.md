6f789b5cf790499865de68ee3f22142e9806791f
jhackett1 <joshua.hackett93@gmail.com>
2017-10-01 17:44:23 +0100

    Added readme

---
+++ README.md
@@ -0,0 +1,14 @@
+Conferoo API
+============
+
+This is an (incomplete) Express application intended for paperless conferences.
+
+API
+===
+
+Conferoo exposes a RESTful API with the following endpoints:
+
+* `GET /events` shows a list of all events in the schedule
+* `GET /speakers` shows a list of people speaking at the conference
+* `GET /posts` shows a list of the blog posts made about the conference
+* `GET /polls` shows a list of the live polls being made at the conference
--- routes/events.js
+++ routes/events.js
@@ -5,15 +5,27 @@ var router = express.Router();
 var Event = require('../models/eventModel');
 
 router.route('/')
+
   //GET Display a list of all events
   .get(function(req, res){
     // Blank query
     var query = {};
+    // Make the list to be queryable by day, time and speaker
+    if (req.query.day) {
+      query.day = req.query.day;
+    }
+    if (req.query.time) {
+      query.time = req.query.time;
+    }
+    if (req.query.speaker) {
+      query.speaker = req.query.speaker;
+    }
     // Make DB query
     Event.find(query, function(err, events){
       res.json(events);
     })
   })
+
   //POST Add a new event to the database
   .post(function(req, res){
     var newEvent = new Event(req.body);
