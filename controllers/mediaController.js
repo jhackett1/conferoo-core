05f715dd10a67cf7ee30937d48ad5ceebb07975d
jhackett1 <joshua.hackett93@gmail.com>
2017-11-05 22:04:00 +0000

    Simplified image handling

--- controllers/mediaController.js
+++ controllers/mediaController.js
@@ -1,6 +1,6 @@
-var sharp = require('sharp');
 var fs = require('fs');
 const sortBy = require('sort-array');
+
 
 // Import AWS SDK for S3 uploads
 var AWS = require('aws-sdk');
@@ -9,33 +9,11 @@ AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 })
+// Create an S3 object to handle uploads
+var conferooBucket = new AWS.S3({params: {Bucket: process.env.S3_BUCKET_NAME}});
+
 
 var mediaController = function(Media){
-
-  // Build filenames
-  var buildFilename = function(rawFilename){
-    // Helper function for date padding
-    function pad(n){return n<10 ? '0'+n : n}
-    let now = new Date();
-    return  pad(now.getHours().toString()) +
-            pad(now.getMinutes().toString()) +
-            pad(now.getSeconds()) + "_" +
-            pad(now.getDate()) +
-            pad(now.getMonth()+1) +
-            pad(now.getFullYear()) + "_" +
-            rawFilename;
-  }
-
-  // Make a 150x150px preview copy of each image in a subfolder
-  var buildPreview = function(filename, originalImagePath, cb){
-    let previewPath = `./public/uploads/previews/${filename}`;
-    sharp(originalImagePath).resize(150, 150).toFile(previewPath, function(err, info){
-      if(err){return cb(err, null);
-      } else {
-        cb(null, previewPath);
-      }
-    });
-  }
 
   // Recieve file uploads, saving them on the server and making a database record
   var post = function(req, res, next){
@@ -48,8 +26,6 @@ var mediaController = function(Media){
     if (!match) return next(new Error("That file type is not supported."));
     // Build a filename for the upload, based on the date
     let filename = buildFilename(upload.originalname);
-    // Create an S3 object to handle uploads
-    var conferooBucket = new AWS.S3({params: {Bucket: process.env.S3_BUCKET_NAME}});
     // Initialise file upload
     conferooBucket.upload({
       ACL: 'public-read',
@@ -61,49 +37,27 @@ var mediaController = function(Media){
       if (err) return next(err);
       console.log(data)
       // Add a record in the database
-      var newMedia = new Media({
-        sources: {
-          full: data.Location,
-          preview: data.Location
-        },
-        s3Key: data.Key,
-        title: upload.originalname,
-        uploadedAt: new Date()
-      });
-      newMedia.save(function(err, newMedia){
-        if(err){return next(err)};
-        res.status(201).json({message: newMedia}).end();
-      });
+      res.status(201).json({message: data}).end();
     })
   }
 
   var get = function(req, res, next){
-    Media.find().sort({uploadedAt: -1}).exec( function(err, events, next){
+    conferooBucket.listObjects({}, function(err, data){
       if(err){return next(err)};
       // Send the results
-      res.status(200).json(events);
+      res.status(200).json(data.Contents);
     })
   }
 
   var deleteSingle = function(req, res, next){
-    Media.findById(req.params.id, function(err, media){
-      if(err){return next(err)};
-      // Create an S3 object to handle uploads
-      var conferooBucket = new AWS.S3({params: {Bucket: process.env.S3_BUCKET_NAME}});
-      // Delete from S3
-      conferooBucket.deleteObject({
-        Key: media.s3Key
-      }, function(err, data) {
-        if (err) return next(err);
-        // Now delete DB record
-        media.remove(function(err){
-          if(err){return next(err)} else {
-            // Thanks user, we're done here
-            res.status(200).json({message: "Media deleted successfully."});
-          }
-        })
-      });
-    })
+    // Delete from S3
+    conferooBucket.deleteObject({
+      Key: req.params.id
+    }, function(err, data) {
+      if (err) return next(err);
+      // Thanks user, we're done here
+      res.status(200).json({message: "Media deleted successfully."});
+    });
   }
 
   // Expose public methods
