8ae779d50671d651a5dfd317adde51614259c0e3
jhackett1 <joshua.hackett93@gmail.com>
2017-11-18 00:44:45 +0000

    Fixed /agenda endpoint

--- controllers/agendaController.js
+++ controllers/agendaController.js
@@ -33,13 +33,18 @@ var agendaController = function(User, Event){
     // Search for and return the user with the specified ID
     User.findById(userId, function(err, user){
       if(err){return next(err)};
-      Event.find({_id: user.agenda})
-        // Sort by programme first, then by time, then themes
-        .sort({programme: 1, time: 1, themes: 1})
-        .exec(function (err, events) {
-          if(err){return next(err)};
-          res.status(200).send(events)
-        });
+      var updatedUser = user;
+      updatedUser.agenda.push(req.body.event)
+      user.save(function(err, updatedUser){
+        if(err){return next(err)};
+        Event.find({_id: updatedUser.agenda})
+          // Sort by programme first, then by time, then themes
+          .sort({programme: 1, time: 1, themes: 1})
+          .exec(function (err, events) {
+            if(err){return next(err)};
+            res.status(200).send(events)
+          });
+      });
     })
   }
 
@@ -53,13 +58,30 @@ var agendaController = function(User, Event){
     // Search for and return the user with the specified ID
     User.findById(userId, function(err, user){
       if(err){return next(err)};
-      Event.find({_id: user.agenda})
-        // Sort by programme first, then by time, then themes
-        .sort({programme: 1, time: 1, themes: 1})
-        .exec(function (err, events) {
-          if(err){return next(err)};
-          res.status(200).send(events)
-        });
+
+      var updatedUser = user;
+
+      var index = updatedUser.agenda.indexOf(req.body.event)
+
+      updatedUser.agenda.splice(index, 1)
+
+      user.save(function(err, updatedUser){
+
+        Event.find({_id: updatedUser.agenda})
+          // Sort by programme first, then by time, then themes
+          .sort({programme: 1, time: 1, themes: 1})
+          .exec(function (err, events) {
+            if(err){return next(err)};
+            res.status(200).send(events)
+          });
+
+      })
+
+
+
+
+
+
     })
   }
 
