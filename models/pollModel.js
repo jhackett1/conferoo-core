0b0572b3fe789877a5fe84aac7eb26e4f0624312
jhackett1 <joshua.hackett93@gmail.com>
2018-01-06 15:01:55 +0000

    Changed poll model

--- models/pollModel.js
+++ models/pollModel.js
@@ -21,7 +21,8 @@ var pollModel = new Schema({
   // Possible responses in an array
   options: {type: Object, default: null},
   // Collate actual responses
-  responses: {type: Object, default: null}
+  responses: {type: Object, default: null},
+  openResponses: {type: Aray, default: null}
 }, { versionKey: false });
 
 module.exports = mongoose.model('poll', pollModel);
